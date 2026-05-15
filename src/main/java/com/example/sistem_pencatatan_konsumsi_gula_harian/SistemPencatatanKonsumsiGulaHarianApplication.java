2be56ec8b083a4bfadb13b22122b754b6d1ee38f
zakkiislamii <mzislami05@gmail.com>
2026-05-15 19:45:25 +0700

    Initial commit

---
+++ src/main/java/com/example/sistem_pencatatan_konsumsi_gula_harian/SistemPencatatanKonsumsiGulaHarianApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistem_pencatatan_konsumsi_gula_harian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemPencatatanKonsumsiGulaHarianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemPencatatanKonsumsiGulaHarianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sistem_pencatatan_konsumsi_gula_harian/SistemPencatatanKonsumsiGulaHarianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistem_pencatatan_konsumsi_gula_harian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemPencatatanKonsumsiGulaHarianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
